d2de2b480b55cb3965a33a78906c7ba5f0004ba5
백승호 <aiden.baek@musinsa.com>
2024-07-23 22:08:35 +0900

    init

---
+++ src/test/java/com/asnisum/SpringExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asnisum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
